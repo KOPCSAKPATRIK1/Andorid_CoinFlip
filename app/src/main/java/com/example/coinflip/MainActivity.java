6fdaf9dc68a1671902d8b7fe7bb45ead39c026ae
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2022-10-13 14:42:20 +0200

    init

--- app/src/main/java/com/example/coinflip/MainActivity.java
+++ app/src/main/java/com/example/coinflip/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.coinflip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView coin;
+    private Button fejBtn;
+    private Button irasBtn;
+    private TextView dobasok;
+    private TextView vereseg;
+    private TextView gyozelem;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
+    }
+
+    private void init()
+    {
+        coin = findViewById(R.id.coin);
+        fejBtn = findViewById(R.id.fej_btn);
+        irasBtn = findViewById(R.id.iras_btn);
+        dobasok = findViewById(R.id.dobasok);
+        vereseg = findViewById(R.id.vereseg);
+        gyozelem = findViewById(R.id.gyozelem);
     }
 }
